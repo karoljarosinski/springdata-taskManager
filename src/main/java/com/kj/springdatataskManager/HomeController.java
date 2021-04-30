3b4748ab1505397b3ebb1d80c83190aeb2b75576
karol <karol.jarosinski91@gmail.com>
2021-04-30 12:55:58 +0200

    oznaczanie zadania jako zrobione

--- src/main/java/com/kj/springdatataskManager/HomeController.java
+++ src/main/java/com/kj/springdatataskManager/HomeController.java
@@ -3,29 +3,29 @@ package com.kj.springdatataskManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class HomeController {
 
-    private final TaskRepositorium taskRepositorium;
+    private final taskRepository taskRepository;
 
-    public HomeController(TaskRepositorium taskRepositorium) {
-        this.taskRepositorium = taskRepositorium;
+    public HomeController(taskRepository taskRepository) {
+        this.taskRepository = taskRepository;
     }
 
     @GetMapping("/")
     public String home(Model model, @RequestParam(required = false) Status status) {
         model.addAttribute("task", new Task());
-        List<Task> tasks = new ArrayList<>();
+        List<Task> tasks;
         if (status != null) {
-            tasks = taskRepositorium.findByStatus(status);
+            tasks = taskRepository.findByStatus(status);
         } else {
-            tasks = taskRepositorium.findAll();
+            tasks = taskRepository.findAll();
         }
         model.addAttribute("tasks", tasks);
         return "home";
@@ -34,12 +34,13 @@ public class HomeController {
     @PostMapping("/add")
     public String add(Task task) {
         task.setStatus(Status.ACTIVE);
-        taskRepositorium.save(task);
+        taskRepository.save(task);
         return "redirect:/";
     }
 
-    @PostMapping("/done")
-    public String done(Task task) {
+    @PostMapping("/done/{id}")
+    public String done(@PathVariable Long id, Task task) {
+        taskRepository.markAsComplete(id);
         return "redirect:/";
     }
 }
--- src/main/java/com/kj/springdatataskManager/taskRepository.java
+++ src/main/java/com/kj/springdatataskManager/taskRepository.java
@@ -4,14 +4,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-public interface TaskRepositorium extends JpaRepository<Task, Long> {
+public interface taskRepository extends JpaRepository<Task, Long> {
 
     List<Task> findByStatus(Status status);
 
+    @Transactional
     @Modifying
-    @Query("update Task t set t.status = 'ACTIVE' where t.id = :id")
+    @Query("update Task t set t.status = 'DONE' where t.id = :id")
     void markAsComplete(@Param(value = "id") Long id);
 }
