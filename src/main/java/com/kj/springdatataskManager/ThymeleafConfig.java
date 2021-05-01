bff0692860afb0c3f43aacefc6019bc155f54721
karol <karol.jarosinski91@gmail.com>
2021-05-01 19:31:26 +0200

    przyznaje sie bez bicia ze dolozony tylko css zostal dla wygladu (poprawki jeszcze nie zostaly przeanalizowane :()

---
+++ src/main/java/com/kj/springdatataskManager/ThymeleafConfig.java
@@ -0,0 +1,14 @@
+package com.kj.springdatataskManager;
+
+import nz.net.ultraq.thymeleaf.LayoutDialect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ThymeleafConfig {
+
+    @Bean
+    public LayoutDialect layoutDialect() {
+        return new LayoutDialect();
+    }
+}
